af28352b6b000641ff8809d3d8624b625d514107
buseugrs <sekerbuse_96@hotmail.com>
2024-06-03 17:03:34 +0300

    fe folder has been added

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,19 +1,10 @@
-import React from "react";
-import { useRoutes } from "react-router-dom";
-import { ThemeProvider } from "@mui/material/styles";
-import {baseTheme} from './assets/global/Theme-variable'
-import Themeroutes from "./routes/Router";
+import './App.css';
 
-const App = () => {
-  const routing = useRoutes(Themeroutes);
-  const theme = baseTheme;
+function App() {
   return (
-    <ThemeProvider theme={theme}>
-      {routing}
-    </ThemeProvider>
+    <div className="App">
+    </div>
   );
-  
-};
-
+}
 
 export default App;
