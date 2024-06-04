69f35abf2f799a8cd46f6d8916ca5f8fed1053c0
buseugrs <sekerbuse_96@hotmail.com>
2024-06-04 17:17:08 +0300

    show bookmark if user is logged in

--- fe/src/components/adverts/Adverts.js
+++ fe/src/components/adverts/Adverts.js
@@ -1,33 +1,35 @@
-import * as React from 'react';
-import AspectRatio from '@mui/joy/AspectRatio';
-import Button from '@mui/joy/Button';
-import Card from '@mui/joy/Card';
-import CardContent from '@mui/joy/CardContent';
-import IconButton from '@mui/joy/IconButton';
-import Typography from '@mui/joy/Typography';
-import BookmarkAdd from '@mui/icons-material/BookmarkAddOutlined';
+import React from "react";
+import { useAuth } from "../../context/auth-context/AuthContext";
+
+import Card from "@mui/joy/Card";
+import CardContent from "@mui/joy/CardContent";
+import IconButton from "@mui/joy/IconButton";
+import Typography from "@mui/joy/Typography";
+import BookmarkAdd from "@mui/icons-material/BookmarkAddOutlined";
+import AspectRatio from "@mui/joy/AspectRatio";
+import Button from "@mui/joy/Button";
 
 const Adverts = ({ advert }) => {
+  const { currentUser } = useAuth();
+
   return (
     <Card sx={{ width: 320 }}>
       <div>
         <Typography level="title-lg">{advert.adTitle}</Typography>
-        <IconButton
-          aria-label={`bookmark ${advert.adTitle}`}
-          variant="plain"
-          color="neutral"
-          size="sm"
-          sx={{ position: 'absolute', top: '0.875rem', right: '0.5rem' }}
-        >
-          <BookmarkAdd />
-        </IconButton>
+        {currentUser && (
+          <IconButton
+            aria-label={`bookmark ${advert.adTitle}`}
+            variant="plain"
+            color="neutral"
+            size="sm"
+            sx={{ position: "absolute", top: "0.875rem", right: "0.5rem" }}
+          >
+            <BookmarkAdd />
+          </IconButton>
+        )}
       </div>
       <AspectRatio minHeight="120px" maxHeight="200px">
-        <img
-          src={advert.adPhoto}
-          loading="lazy"
-          alt={advert.adTitle}
-        />
+        <img src={advert.adPhoto} loading="lazy" alt={advert.adTitle} />
       </AspectRatio>
       <CardContent orientation="horizontal">
         <div>
@@ -41,7 +43,7 @@ const Adverts = ({ advert }) => {
           size="md"
           color="primary"
           aria-label={`Explore ${advert.adTitle}`}
-          sx={{ ml: 'auto', alignSelf: 'center', fontWeight: 600 }}
+          sx={{ ml: "auto", alignSelf: "center", fontWeight: 600 }}
         >
           Explore
         </Button>
